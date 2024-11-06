44be1132a45206879e0f208ab559e20851b6773e
D1ma750 <suter213@mail.ru>
2024-11-06 21:32:06 +0400

    lab

--- lab2/main.py
+++ lab2/main.py
@@ -76,9 +76,12 @@ def main():
     arguments = parsing_arguments()
     try:
         download_images(arguments.keyword, arguments.number_of_images, arguments.imgdir)
-        create_annotation(arguments.imgdir,arguments.annotation_file)
-        my_iterator = ImageIterator(arguments.annotation_file)
+        create_annotation(arguments.imgdir,arguments.file_with_annotation)
+        my_iterator = ImageIterator(arguments.file_with_annotation)
         for image in my_iterator:
             print(image)
     except Exception as e:
         print(f"Error: {e} ")
+
+if __name__=="__main__":
+    main()
