ce9cf0f84a10732e825ceba02173bccf0a9dd1da
D1ma750 <suter213@mail.ru>
2024-11-21 12:56:45 +0400

    last variant

--- lab3/main.py
+++ lab3/main.py
@@ -1,25 +1,27 @@
 import cv2
 import argparse
 
-from work_with_image import colors_of_histogram, creating_histogram, changing_image_size, show_image
+from work_with_image import create_hist_of_channels, show_histogram, changing_image_size, show_image
 
 
 def parsing_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("image_path", type=str, help="Path to image")
-    parser.add_argument("new_image_path", type=str, help="Path to load new image")
-    parser.add_argument("height", type=int, help="Height of new image")
-    parser.add_argument("width", type=int, help="Width of new image")
+    parser.add_argument("image_path", type = str, help = "Path to image")
+    parser.add_argument("new_image_path", type = str, help = "Path to load new image")
+    parser.add_argument("height", type = int, help = "Height of new image")
+    parser.add_argument("width", type = int, help = "Width of new image")
     arguments = parser.parse_args()
     return arguments
+
+
 def main():
     arguments = parsing_arguments()
     try:
         image = cv2.imread(arguments.image_path)
         print(image.shape)
-        blue, green, red= colors_of_histogram(image)
-        creating_histogram(blue, green, red)
-        new_image=changing_image_size(image, arguments.width, arguments.height)
+        blue, green, red = create_hist_of_channels(image)
+        show_histogram(blue, green, red)
+        new_image = changing_image_size(image, arguments.width, arguments.height)
         print(new_image.shape)
         show_image(image, new_image)
         cv2.imwrite(arguments.new_image_path, new_image)
--- lab3/work_with_image.py
+++ lab3/work_with_image.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def colors_of_histogram(image:np.ndarray)->tuple:
+def create_hist_of_channels(image: np.ndarray) -> tuple:
     blue = cv2.calcHist([image], [0], None, [256], [0, 255])
     green = cv2.calcHist([image], [1], None, [256], [0, 255])
     red = cv2.calcHist([image], [2], None, [256], [0, 255])
-    return blue,green,red
+    return blue, green, red
 
 
-def creating_histogram(blue:np.ndarray, green:np.ndarray, red:np.ndarray)->None:
-    plt.figure(figsize=(10, 5))
+def show_histogram(blue: np.ndarray, green:np.ndarray, red:np.ndarray) -> None:
+    plt.figure(figsize = (10, 5))
     plt.plot( blue, label='Синяя линия', color='blue')
     plt.plot( green, label='Зеленая линия', color='green')
     plt.plot( red, label='Красная линия', color='red')
@@ -24,12 +24,11 @@ def creating_histogram(blue:np.ndarray, green:np.ndarray, red:np.ndarray)->None:
     plt.show()
 
 
-def changing_image_size(image:np.ndarray, width:int, height:int)->np.ndarray:
-    new_image=cv2.resize(image, (height, width))
-    return new_image
+def changing_image_size(image: np.ndarray, width: int, height: int) -> np.ndarray:
+    return cv2.resize(image, (height, width))
 
 
-def show_image(image:np.ndarray, new_image:np.ndarray)->None:
+def show_image(image:np.ndarray, new_image:np.ndarray) -> None:
     cv2.imshow('image', image)
     cv2.imshow('image_with_new_size', new_image)
     cv2.waitKey(0)
